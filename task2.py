ac5a09be0f24756766d1d2b74f44a244fb347ea0
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-02-04 16:20:44 +0100

    new5

--- task2.py
+++ task2.py
@@ -1,8 +1,10 @@
 from prettytable import PrettyTable
 
 table = PrettyTable()
+table.align = "l"
 
 table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
 table.add_column("Type", ["Electric", "Water", "Fire"])
 
 print(table)
+print(table.align)
