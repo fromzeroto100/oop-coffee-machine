f0b22d6acdc217cbbab59fb739c8c57b8c8ba5e1
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-02-04 14:41:18 +0100

    new1

--- task1.py
+++ task1.py
@@ -2,5 +2,9 @@ from turtle import Turtle, Screen
 
 tyson = Turtle()
 
+tyson.color("red")
+tyson.shape("turtle")
+tyson.fd(100)
 my_screen = Screen()
 print(my_screen.canvwidth)
+my_screen.exitonclick()
