351d76a49af91e42649343eca392a297ce6bf82f
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-02-06 19:49:27 +0100

    new2

--- main1.py
+++ main1.py
@@ -4,9 +4,19 @@ from money_machine import MoneyMachine
 
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
+menu = Menu()
 
 
 is_on = True
 
 while is_on:
+    options = menu.get_items()
     choice = input("What would you like? latte/espresso/cappuccino: ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        money_machine.report()
+        coffee_maker.report()
+
+    else:
+            
